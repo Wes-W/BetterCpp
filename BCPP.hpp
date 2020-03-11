bd3792b535ffafa77fd192b0818e9100a6401bf4
Wesley <wesathome@outlook.com>
2020-03-11 08:24:35 -0400

    Create BCPP.hpp

---
+++ BCPP.hpp
@@ -0,0 +1,26 @@
+//Types
+#define nothing void
+#define onoff bool
+#define num int
+#define numdec float
+#define largenumdec double
+
+//Basic symobols
+#define begin {
+#define end }
+#define beginzone (
+#define endzone )
+#define endline ;
+#define zone :
+#define another , 
+#define sendback return
+#define add +
+#define set =
+#define make new
+#define pointer *
+#define access .
+#define pointeraccess ->
+
+//Classes
+#define classroom class
+#define notpublic private
